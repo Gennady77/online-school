a69bd0b3850be2045f46b479568e68a8941f8d66
Gennadii_Kondratev <gipper2006@gmail.com>
2021-09-19 10:18:20 +0300

    [develop] render course data in the table - apply ngFor - set data in app component - create type CourseData

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {CourseData} from "./types";
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,25 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'online-school';
+  public courseList: CourseData[] = [{
+    id: 1,
+    title: 'Алгоритмы',
+    date: new Date('2021-09-01')
+  }, {
+    id: 2,
+    title: 'Computer Science',
+    date: new Date('2021-09-10')
+  }, {
+    id: 3,
+    title: 'Математическая статистика',
+    date: new Date('2021-10-01')
+  }, {
+    id: 4,
+    title: 'Политическая экономика',
+    date: new Date('2021-10-15')
+  }, {
+    id: 5,
+    title: 'CSS методология',
+    date: new Date('2021-10-21')
+  }];
 }
---
+++ src/app/types/index.ts
@@ -0,0 +1,5 @@
+export interface CourseData {
+  id: number,
+  title: string,
+  date: Date
+}
